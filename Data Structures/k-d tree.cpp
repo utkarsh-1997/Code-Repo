b7fcd5aa9e3ddbe7b8f8ce77861f8c814a581d6a
Utkarsh Singh <utkarshsinghoct1997@gmail.com>
2018-07-05 00:35:24 +0530

    Added k-d tree

---
+++ Data Structures/k-d tree.cpp
@@ -0,0 +1,14 @@
+// Refer to:
+// https://github.com/utkarsh1097/DBMS-Assignments/blob/master/Assignment%203/bestfirstsearch.cpp
+// for the implementation of the k-d tree. Please modify the main() function according to your needs.
+// The code is pretty neat and it will not be difficult to understand. Still, will add comments in future if needed.
+
+//Just for writing something, I will write a "Hello World" program below!
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
